7d21c831746aefb4cc009a41d65c59093d8f4a00
Selina <selina@kionnali.com>
2024-08-22 14:55:46 +0100

    Reset project with blank template

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const navigator = createStackNavigator({
-}, {
-  initialRouteName: "",
-  defaultNavigationOptions: {
-    title: "Kionnali"
-  }         
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-export default createAppContainer(navigator);
