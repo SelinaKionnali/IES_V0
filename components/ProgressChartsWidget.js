56ab66d0e83736f1b895d8aa47bdec19d563dbc0
Selina <selina@kionnali.com>
2024-08-29 13:08:22 +0100

    ProgressChartWidget. js created

---
+++ components/ProgressChartsWidget.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import SolarProgressChart from './SolarProgressChart';
+import ThermalProgressChart from './ThermalProgressChart';
+
+const ProgressChartsWidget = () => {
+  return (
+    <View style={styles.container}>
+      <SolarProgressChart />
+      <ThermalProgressChart />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row', // Arrange items horizontally
+    justifyContent: 'space-around', // Space them evenly
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#183564',
+    borderRadius: 20,
+  },
+});
+
+export default ProgressChartsWidget;
