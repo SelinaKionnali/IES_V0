805cf7010a879d8cbecbccdbf879989f81e17373
Selina <selina@kionnali.com>
2024-09-05 18:55:39 +0100

    editing the timestamp format

--- charts/solarEnergyUsage.js
+++ charts/solarEnergyUsage.js
@@ -1,23 +1,43 @@
+// Line Chart
 // This chart will show how much solar energy was generated (wattIn) and used (wattOut) throughout the day. 
 // It gives users a sense of their solar system’s performance at different times of the day.
 
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import {  View, StyleSheet, Dimensions } from 'react-native'
+import { LineChart } from 'react-native-chart-kit'
+import { solarData } from '../data/solarData'
 
+const chartConfig = {
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientToOpacity: 0,
+    color: (opacity = 1) => `rgba(255, 213, 104, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    useShadowColorFromDataset: false // optional
+  };
 
 const SolarEnergyUsage = () => {
     return (
         <View>
-            <Text style={styles.text}>solar usage chart</Text>
+            <LineChart
+                data={{
+                    labels: solarData.daily.map((data) => 
+                        moment(data.timestamp).format('h:mm A') 
+                        ),
+                    datasets: [
+                         { data: solarData.daily.map((data) => data.wattIn), label: 'Watt In' },
+                        { data: solarData.daily.map((data) => data.wattOut), label: 'Watt Out' }
+                    ]
+                    }}
+                    width={Dimensions.get('window').width - 40}
+                    height={220}
+                    yAxisSuffix="W"
+                    chartConfig={chartConfig}
+/>
         </View>
     )
 }
 
 styles = StyleSheet.create ({
-    text: {
-        fontFamily: 'Text-Regular',
-        color: '#FFF1CF'
-    }
 });
 
 export default SolarEnergyUsage;
