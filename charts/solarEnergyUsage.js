763d45dec90e6a10fa5fd2d03e3d6f8ae2b4f941
Selina <selina@kionnali.com>
2024-09-05 20:17:49 +0100

    timestamp label updated and chart design edits

--- charts/solarEnergyUsage.js
+++ charts/solarEnergyUsage.js
@@ -3,11 +3,13 @@
 // It gives users a sense of their solar system’s performance at different times of the day.
 
 import React from 'react'
-import {  View, StyleSheet, Dimensions } from 'react-native'
+import {  View, StyleSheet, Dimensions, Text } from 'react-native'
 import { LineChart } from 'react-native-chart-kit'
 import { solarData } from '../data/solarData'
 import dayjs from 'dayjs'
-import {RelativeTime} from 'dayjs/plugin/relativeTime'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime); // Extend Day.js with the plugin
 
 const chartConfig = {
     backgroundGradientFromOpacity: 0,
@@ -20,26 +22,42 @@ const chartConfig = {
 const SolarEnergyUsage = () => {
     return (
         <View>
+            <Text style={styles.text}>Solar Energy Usage (Watts)</Text>
             <LineChart
                 data={{
-                    labels: solarData.daily.map((data) => 
-                        moment(data.timestamp).format('h:mm A') 
-                        ),
-                    datasets: [
-                         { data: solarData.daily.map((data) => data.wattIn), label: 'Watt In' },
-                        { data: solarData.daily.map((data) => data.wattOut), label: 'Watt Out' }
-                    ]
+                    labels: solarData.daily.map((data) => {
+                        const date = dayjs(data.timestamp)
+                        return date.format('hh.mm A'); // Simply show day of the week (Mon, Tue, Wed)
+                 } ),
+                 datasets: [
+                    {
+                      data: solarData.daily.map((data) => data.wattIn),
+                      color: (opacity = 1) => `rgba(255, 213, 104, ${opacity})`, 
+                      label: 'Watt In'
+                    },
+                    {
+                      data: solarData.daily.map((data) => data.wattOut),
+                      color: (opacity = 1) => `rgba(196, 154, 207, ${opacity})`,
+                      label: 'Watt Out'
+                    }
+                  ],
+                  legend: ['Watt In', 'Watt Out'] // Labels for the lines in the chart
                     }}
                     width={Dimensions.get('window').width - 40}
                     height={220}
                     yAxisSuffix="W"
                     chartConfig={chartConfig}
-/>
+                    
+            />
         </View>
     )
 }
 
 styles = StyleSheet.create ({
+    text: {
+        color: '#FFF1CF',
+        fontFamily: 'Text-Regular'
+    }
 });
 
 export default SolarEnergyUsage;
