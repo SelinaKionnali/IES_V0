19d1b28ed71f67c66f201f581a412c3a778a9f7e
Selina <selina@kionnali.com>
2024-09-05 09:32:30 +0100

    GradientBackground.js created based on figma design

---
+++ components/GradientBackground.js
@@ -0,0 +1,27 @@
+// This is a reusable gradient background component that can be imported and applied wherever needed
+// Will import to the app.js file to be used in this app universally
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const GradientBackground = ({ children }) => {
+  return (
+    <LinearGradient
+      colors={['#0F2241', '#304D76', '#324F77', '#4C698B']}
+      style={styles.background}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+});
+
+export default GradientBackground;
