e2b4612238e1183741ebb19965fde3f77ecd10fb
Selina <selina@kionnali.com>
2024-09-05 12:11:07 +0100

    fixed sizing issue within widget by adding min and max widths for the component containers

--- components/ProgressChartsWidget.js
+++ components/ProgressChartsWidget.js
@@ -9,8 +9,12 @@ import ThermalProgressChart from './ThermalProgressChart';
 const ProgressChartsWidget = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.chartContainer}>
       <SolarProgressChart />
+      </View>
+      <View style={styles.chartContainer}> 
       <ThermalProgressChart />
+      </View>
     </View>
   );
 };
@@ -18,13 +22,27 @@ const ProgressChartsWidget = () => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row', // Arrange items horizontally
-    justifyContent: 'space-around', // Space them evenly
+    justifyContent: 'space-between', // Space them evenly
     alignItems: 'center',
-    padding: 20,
+    padding: 10,
     backgroundColor: '#21436B',
     borderRadius: 20,
     marginTop: 40,
     marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+
+    // Shadow for iOS
+    shadowColor: '#000',  // Color of the shadow
+    shadowOffset: { width: 0, height: 10 },  // X and Y offset for the shadow
+    shadowOpacity: 0.25,  // Transparency of the shadow
+    shadowRadius: 10,  // Blurring radius
+  },
+  chartContainer: {
+    flex: 1,  // Allows both charts to take equal space
+    minWidth: 150,  // Set a minimum width to avoid the charts shrinking too much
+    maxWidth: '50%',  // Ensure each chart takes up to 50% of the container
+    alignItems: 'center',  // Center the charts in their container
   },
 });
 
