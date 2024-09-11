50ddcd58f26c93c4d5c06277525b6b76b0d4229d
Selina <selina@kionnali.com>
2024-09-11 16:12:23 +0100

    violet square animation with bg colour added into functionality and reset button

--- components/Reanimated.js
+++ components/Reanimated.js
@@ -4,15 +4,22 @@ import Animated, { useSharedValue } from 'react-native-reanimated';
 
 export default function ReanimatedComponent() {
     const width = useSharedValue(100);
+    const backgroundColor = useSharedValue('violet')
 
     const handlePress = () => {
         if (width.value < 350) {
             width.value = width.value + 50;
-        }
-       
-    }
+            backgroundColor.value = 'blue'
+        } else if (width.value = 350) {
+            backgroundColor.value = 'red'
+
+        } 
+        };
+
     const reset = () => {
         width.value = 100;
+        backgroundColor.value = 'violet'
+
     }
 
     return (
@@ -21,7 +28,7 @@ export default function ReanimatedComponent() {
             style={{
                 width,
                 height: 100,
-                backgroundColor: 'violet',
+                backgroundColor,
             }}
         />
         <Button onPress={handlePress} title='Click me' />
