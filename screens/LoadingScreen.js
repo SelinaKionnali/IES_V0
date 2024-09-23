adde295136072330cf19d26fff5dde02504dc11c
Selina <selina@kionnali.com>
2024-09-23 11:09:38 +0100

    created initial loading screen file

---
+++ screens/LoadingScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const LoadingScreen () => {
+
+    return (
+        <View>
+            <Text style={styles.text}>Loading Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'FFF1CF',
+        fontFamily: 'Text-Regular',
+        fontSize: 30
+    }
+})
