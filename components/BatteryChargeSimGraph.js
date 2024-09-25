de0c56a304649cb3f1c82db9b9a26c3b55ae64d1
Selina <selina@kionnali.com>
2024-09-25 14:02:50 +0100

    Copied over the line graph from power page, now need to map over the simulated data

--- components/BatteryChargeSimGraph.js
+++ components/BatteryChargeSimGraph.js
@@ -2,6 +2,21 @@ import React from 'react'
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import { LineChart } from 'react-native-chart-kit'
 import BaseThermalLoad from '../data/BaseThermalLoad.json'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+
+
+dayjs.extend(relativeTime); // Extend Day.js with the plugin
+
+const chartConfig = {
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientToOpacity: 0,
+    color: (opacity = 1) => `rgba(255, 213, 104, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    fillShadowGradient: '#C49ACF',
+    fillShadowGradientOpacity: 1,
+  };
 
 
 const BatteryChargeSimGraph = () => {
@@ -12,6 +27,33 @@ const BatteryChargeSimGraph = () => {
                 <Text style={styles.text}>Your battery level over time</Text>
             </View>
             <View style={styles.lineGraphContainer}>
+            <LineChart
+                data={{
+                    labels: solarData.daily.map((data) => {
+                        const date = dayjs(data.timestamp)
+                        return date.format('hh.mm A'); // Simply show day of the week (Mon, Tue, Wed)
+                 } ),
+                 datasets: [
+                    {
+                      data: solarData.daily.map((data) => data.wattIn),
+                      color: (opacity = 1) => `rgba(255, 213, 104, ${opacity})`, 
+                      label: 'Watt In'
+                    },
+                    {
+                      data: solarData.daily.map((data) => data.wattOut),
+                      color: (opacity = 1) => `rgba(196, 154, 207, ${opacity})`,
+                      label: 'Watt Out'
+                    }
+                  ],
+                  legend: ['Watt In', 'Watt Out'] // Labels for the lines in the chart
+                    }}
+                    width={Dimensions.get('window').width - 40}
+                    height={220}
+                    yAxisSuffix="W"
+                    chartConfig={chartConfig}
+                    
+            />
+
             </View>
 
         </View>
